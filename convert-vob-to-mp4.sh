e2b6cb601d0bd6681d44d0bdfde9bd8d7ca313ad
Sean Crosby <seanmcrosby@gmail.com>
2016-08-15 06:15:28 -0600

    bug fixes

--- convert-vob-to-mp4.sh
+++ convert-vob-to-mp4.sh
@@ -14,6 +14,10 @@ DST=$2
 
 for f in $SRC/*.VOB
 do
-  echo "Processing $f file..."
-  ~/bin/ffmpeg -i $f -ac 2 -ab 128k -vcodec libx264 -crf 20 -threads 0 "${DST}/${f%.*}.mp4"  
+  # strip off directories
+  s=${f##*/}
+
+  # convert video
+  echo "### Processing $f file... (will save to ${DST}/${s%.*}.mp4)"
+  ~/bin/ffmpeg -i $f -ac 2 -ab 128k -vcodec libx264 -crf 20 -threads 0 "${DST}/${s%.*}.mp4"  
 done
